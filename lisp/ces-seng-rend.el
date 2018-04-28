79516761bc8cfd3766886bbfcafed178cef639b3
alexjgriffith <griffitaj@gmail.com>
2018-04-28 02:19:07 -0400

    Completed rendering timelines.

--- lisp/ces-seng-rend.el
+++ lisp/ces-seng-rend.el
@@ -37,29 +37,57 @@
 
 (defvar ces-seng-rend-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "M-n") #'ces-seng-render-move-to-next-message)
-    (define-key map (kbd "M-p") #'ces-seng-render-move-to-previous-message)
-    (define-key map (kbd "RET") #'ces-seng-render-eval-at-point)
-    (define-key map (kbd "c") #'ces-seng-render-eval-at-point)
+    (define-key map (kbd "M-n") #'ces-seng-rend-move-to-next-message)
+    (define-key map (kbd "k") #'ces-seng-rend-move-to-next-message)
+    (define-key map (kbd "M-p") #'ces-seng-rend-move-to-previous-message)
+    (define-key map (kbd "j") #'ces-seng-rend-move-to-previous-message)
+    (define-key map (kbd "RET") #'ces-seng-rend-eval-at-point)
     (define-key map (kbd "g") #'undefined)
     map))
 
-(define-derived-mode ces-seng-render-mode special-mode "Seng"
+(defvar ces-seng-rend-display-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "q") #'kill-this-buffer)
+    map))
+
+(defun ces-seng-rend-move-to-next-message ()
+  (interactive)
+  (ces-seng-rend-goto-next-byline (point)))
+
+(defun ces-seng-rend-move-to-previous-message ()
+  (interactive)
+  (ces-seng-rend-goto-previous-byline (point)))
+
+(defun ces-seng-rend-eval-at-point ()
+  (interactive)
+  (ces-seng-rend-display-details (point)))
+
+(define-derived-mode ces-seng-rend-mode special-mode "SengTL"
   "Major mode for seng timeline style rendering."
-  (read-only-mode 't)
-  ;;(setq font-lock-defaults '(ces-seng-render-mode-highlights))
-  )
+  (read-only-mode 't))
+
+(define-derived-mode ces-seng-rend-display-mode special-mode "SengD"
+  "Major mode for seng timeline style rendering."
+  (read-only-mode 't))
 
 ;; From ces-seng-render
 
 (defvar ces-seng-rend-test-entities-alist
   '((10 ((deer)
-         (likes :hash #s(hash-table test equal data ("Grass" 1 "Forests" 1 "Blue Shirts" 1 "Long walks on the Beach" 1 "Wolves" -1 "Red Shirts" -1 "You" -1))
+         (interactions :hash #s(hash-table test equal data ("Pet Deer" 11 "Chase Deer" 12)))
+         (likes :hash #s(hash-table test equal data ("People who pet deer" t "Grass" 1 "Forests" 1 "Blue Shirts" 1 "Long walks on the Beach" 1 "Wolves" -1 "Red Shirts" -1 "You" -1))
                 :current-happiness 100)
          (named :sname "Deer" :rname "@deer@1.loc1" :dname "Deer"
                 :description "A furry deer friend. Both lovable and delicions; the best combination."
-                :pronoun "it"
-                :att #s(hash-table test equal data ("fury" t "deer" t "woodland" t))))))
+                :pronoun "It"
+                :att #s(hash-table test equal data ("fury" t "deer" t "woodland" t)))))
+    (11 ((interaction :name "Pet Deer" :effect 'attributes
+                      :hash #s(hash-table test equal date ("People who pet deer" 1
+                                                           "People who chase deer" -1)))))
+    (12 ((interaction :name "Chase Deer" :effect 'attributes
+                      :hash #s(hash-table test equal date ("People who pet deer" -1
+                                                           "People who chase deer" 1)))         
+         )))
   "Should match the output of (ces-components-f (ces-e2c-f enti-id))")
 
 (defvar ces-seng-rend-test-message-list
@@ -70,7 +98,7 @@
 (defun ces-seng-rend-test ()
   (let ((message-buffer "*ces-seng-rend-test*"))
    (with-current-buffer (get-buffer-create message-buffer)
-    (ces-seng-render-mode)   
+    (ces-seng-rend-mode)   
     (ces-seng-rend-render-message (elt ces-seng-rend-test-message-list 0) message-buffer)
     (ces-seng-rend-render-message (elt ces-seng-rend-test-message-list 1) message-buffer)
     (ces-seng-rend-render-message (elt ces-seng-rend-test-message-list 2) message-buffer)
@@ -89,6 +117,7 @@
          (named (ces-utils-comp-get-plist components 'named))
          (dname (plist-get named :dname))
          (rname (plist-get named :rname))
+         (pronoun (plist-get named :pronoun))
          (description (plist-get named :description))
          (hash (ces-utils-comp-get-value components 'likes :hash))
          likes dislikes)
@@ -97,122 +126,76 @@
     ;;(nreverse likes)
     ;;(nreverse dislikes)
     (message "likes: %s" likes)
-    (concat (propertize dname 'face 'warning)
-            " ("
-            (propertize rname 'face 'font-lock-string-face) ")\n"
-            (propertize description 'face font-lock-builtin-face)
-            "\n\n"
-            (propertize rname 'face 'font-lock-string-face)
-            " likes "
-            (mapconcat (lambda (str) (propertize str 'face font-lock-keyword-face))
-                       (cdr likes) ", ")
-            " & "
-            (propertize (car likes) 'face  font-lock-keyword-face)
-            ".\nThey dislike "
-            (mapconcat (lambda (str) (propertize str 'face font-lock-keyword-face))
-                       (cdr dislikes) ", ")
-            " & "
-            (propertize (car dislikes) 'face font-lock-keyword-face))))
+    (propertize
+     (concat (propertize dname 'face 'warning)
+             " ("
+             (propertize rname 'face 'font-lock-string-face)
+             ")\n============\n\n"
+             (propertize description 'face font-lock-builtin-face)
+             (format "\n%s likes " pronoun)
+             (mapconcat (lambda (str) (propertize str 'face font-lock-keyword-face))
+                        (cdr likes) ", ")
+             " & "
+             (propertize (car likes) 'face  font-lock-keyword-face)
+             "."
+             (format "\n%s dislikes " pronoun)
+             (mapconcat (lambda (str) (propertize str 'face font-lock-keyword-face))
+                        (cdr dislikes) ", ")
+             " & "
+             (propertize (car dislikes) 'face font-lock-keyword-face)
+             ".\n\n"
+             (propertize "Interactions" 'face font-lock-builtin-face)
+             "\n============\n"
+             )
+     'header-section t)))
 
-(defun ces-seng-rend-display-details ()
-  (interactive)
-  (let ((props (text-properties-at (point))))
+(defun ces-seng-rend-display-details (point)
+  (let ((props (text-properties-at point))
+        (inhibit-read-only t))
    (when (and (member 'buf-name props) (member 'entity props))
      (with-current-buffer (get-buffer-create (plist-get props 'buf-name))
        (delete-region (point-min) (point-max))
        (insert (ces-seng-rend-entity (plist-get props 'entity)
                                      (plist-get props 'components)))
-       (display-buffer (current-buffer))))))
+       (ces-seng-rend-display-mode)
+       (switch-to-buffer (current-buffer))))))
 
 ;; need to add an 'entity, components and buf-name property to the byline
 
-(defun ces-seng-rend-byline (dname rname when entity &optional components)
-  (propertize
-   (concat "| " (propertize (concat  dname) 'face 'warning) " (" rname") "
-           (propertize (concat when "\n") 'timeline-time t 'time-length (length when)))
-   'entity entity
-   'components components
-   'rname rname))
-
-(defun ces-seng-rend-create-message (message)
+(defun ces-seng-rend-create-message (message-buffer message)
   (let* ((body (plist-get message :body))
          (ent-id (plist-get message :ent-id))
          (components (cadr (assoc ent-id ces-seng-rend-test-entities-alist)))
          (dname (ces-utils-comp-get-value components 'named :dname))
          (rname (ces-utils-comp-get-value components 'named :rname))
+         (when  (plist-get message :when))         
          (relative-when  (ces-seng-rend-timestamp-relative (plist-get message :when)))
-         (byline (ces-seng-rend-byline dname rname relative-when
-                                       ent-id components)))
-    (concat body"\n" byline)))
+         (str (concat (propertize (concat body "\n") 'body-section t)
+                      (propertize "| " 'byline-start t)
+
+                      (propertize
+                       (concat (propertize (concat  dname) 'face 'warning)
+                               " (" rname") ")
+                       'byline-section t)
+                      (propertize relative-when 'time-section t)
+                      "\n"
+                      (propertize "\n" 'end-section t))))
+    (propertize
+     str
+     'entity ent-id
+     'components components
+     'rname rname
+     'when when
+     'length (length str)
+     'buf-name (concat message-buffer "-" rname))))
 
 (defun ces-seng-rend-render-message(message message-buffer)
   (with-current-buffer (get-buffer-create message-buffer)
     (save-excursion
       (goto-char (point-min))
-      (let ((inhibit-read-only t)
-            (when  (plist-get message :when))
-            (str (ces-seng-rend-create-message message)))
-        (insert
-         (propertize str
-                     'when when
-                     'length (length str)
-                     'buf-name
-                     (concat message-buffer
-                             "-"
-                             (get-text-property (length str) 'rname str)))
-         (propertize "\n" 'inbetween t))))))
-
-(defun ces-seng-rend-unpause-message-time (&optional point)
-  (let* ((point (or point (point)))
-         (reg (ces-seng-rend-message-region point))
-         (start (car reg))
-         (end (cdr reg))
-         (when (plist-get (text-properties-at point) 'when))
-         (current (current-time)))
-    (add-text-properties start end 'when
-                         (time-subtract current (time-subtract current when)))))
-
-(defun ces-seng-rend-update-message-time (&optional point)
-  (let* ((point (or point (point)))
-         (region (ces-seng-rend-when-region))
-         (start (car region))
-         (end (cdr region))         
-         (when (get-text-property point 'when))
-         (relative-when (ces-seng-rend-timestamp-relative when))
-         (str (propertize (concat relative-when "\n")
-                          'timeline-time t 'time-length (length relative-when)))
-         (new-time-len (length str))
-         (message-len (get-text-property end 'length))
-         (old-time-len (get-text-property end 'time-length))
-         (new-message-len (+ message-len (- new-time-len old-time-len)))
-         (inhibit-read-only t))
-      (ces-seng-rend-set-property-for-message point 'length new-message-len)
-      (goto-char end)
-      (delete-region start (+ end 1))
-      (insert str)))
-
-(defun ces-seng-rend-set-property-for-message (point property value)
-  (let* ((region (ces-seng-rend-message-region point))
-         (start (car region))
-         (end (cdr region)))
-    (add-text-properties start end (list property value))))
-
-(defun ces-seng-rend-when-region (&optional point)
-  (let* ((point (or point (point)))
-        (next-prop (next-single-property-change point 'inbetween)))
-    (when (not (plist-get (text-properties-at next-prop) 'inbetween))
-      (setq next-prop  (next-single-property-change next-prop 'inbetween)))
-    (let ((length (plist-get (text-properties-at (- next-prop 1)) 'time-length)))      
-      (cons (- next-prop length 1)  (- next-prop 1) ))))
-
-
-(defun ces-seng-rend-message-region (&optional point)
-  (let* ((point (or point (point)))
-        (next-prop (next-single-property-change point 'inbetween)))
-    (when (not (plist-get (text-properties-at next-prop) 'inbetween))
-      (setq next-prop  (next-single-property-change next-prop 'inbetween)))
-    (let ((length (plist-get (text-properties-at (- next-prop 1)) 'length)))
-      (cons (- next-prop length)  (- next-prop 1) ))))
+      (let ((inhibit-read-only t)            
+            (str (ces-seng-rend-create-message message-buffer message)))
+        (insert str)))))
 
 (defmacro ces-seng-rend-timestamp-make-to-string-alist (seconds-per-hour)
   `(defun ces-seng-rend-timestamp-to-string-alist (diff)
@@ -223,30 +206,144 @@
             `((>= diff ,time) ,stamp)))
                 (reverse
                  `((0 . "now")
-                   (7 . "30 min ago")
+                   (,(floor (/ seconds-per-hour 2) ) . "30 min ago")
+                   (,seconds-per-hour . "1 hour ago")
                    ,@(mapcar (lambda(hours) (cons (* hours seconds-per-hour)
                                                   (format "%s hours ago" hours)))
-                             '(1 2 3 4 5 6 7 8 9 10 11 12 13
+                             '(2 3 4 5 6 7 8 9 10 11 12 13
                                  14 15 16 17 18 19 20 21 22 23))
+                   (,(* 24 seconds-per-hour) . "1 day ago")
                    ,@(mapcar (lambda(days) (cons (* days seconds-per-hour 24)
                                                  (format "%s days ago" days)))
-                             '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+                             '(2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
                                  21 22 23 24 25 26 27 28 29))
+                   (,(* 24 30 seconds-per-hour) . "1 months ago")
                    ,@(mapcar (lambda(days) (cons (* days seconds-per-hour 24 30)
                                                  (format "%s months ago" days)))
-                             '(1 2 3 4 5 6 7 8 9 10 11))
+                             '(2 3 4 5 6 7 8 9 10 11))
+                   (,(* 24 30 12 seconds-per-hour) . "1 months ago")
                    ,@(mapcar (lambda(days) (cons (* days seconds-per-hour 24 30 12)
                                                  (format "%s years ago" days)))
-                             '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+                             '(2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
                                  21 22 23 24 25 26 27 28 29))
                    ))))))
 
 (defun ces-seng-rend-timestamp-relative (timestamp &optional current-time)
- (unless (functionp 'ces-seng-timestamp-to-string-alist)
-   (ces-seng-timestamp-make-to-string-alist 15))
+ (unless (functionp 'ces-seng-rend-timestamp-to-string-alist)
+   (ces-seng-rend-timestamp-make-to-string-alist 15))
  (let* ((now (or current-time (current-time)))
         (time-diff (float-time (time-subtract now timestamp))))
-   (ces-seng-timestamp-to-string-alist time-diff)))
+   (ces-seng-rend-timestamp-to-string-alist time-diff)))
+
+(defun ces-seng-rend-iter-over-times (fun)
+  (let ((next-loc (ces-seng-rend-next-prop-t 1 'byline-start))
+        (counter 0))
+    (while next-loc                     
+      (funcall fun next-loc)
+      (setq counter (+ counter 1))
+      (setq next-loc  (ces-seng-rend-next-prop-t next-loc 'byline-start)))
+    counter))
+
+(defun ces-seng-rend-update-all-times (&optional buffer override)
+  (with-current-buffer (get-buffer-create (or buffer (current-buffer)))
+    (let ((current (current-time)))
+      (ces-seng-rend-iter-over-times
+       (lambda (point)
+         (ces-seng-rend-replace-timer-region 
+          point
+          (or
+           override
+           (ces-seng-rend-timestamp-relative (get-text-property point 'when)))))))))
+
+(defun ces-seng-rend-pause-all-times (&optional buffer)
+  (with-current-buffer  (get-buffer-create  (or buffer (current-buffer)))
+    (let ((current (current-time)))
+      (ces-seng-rend-iter-over-times
+       (lambda (point)
+         (ces-seng-rend-replace-message-properties point `(paused ,current)))))))
+
+(defun ces-seng-rend-unpause-all-times (&optional buffer)
+  (with-current-buffer (get-buffer-create (or buffer (current-buffer)))
+    (let ((current (current-time)))
+      (ces-seng-rend-iter-over-times
+       (lambda (point)
+         (let* ((when (get-text-property point 'when))
+                (paused (get-text-property point 'paused))
+                (new-when (time-add  when (time-subtract current paused))))
+           (when paused
+             (ces-seng-rend-replace-message-properties
+              point `(paused nil when ,new-when)))))))))
+
+(defun ces-seng-rend-replace-message-properties (point properties)  
+  (let* ((region (ces-seng-rend-message-region point))
+         (start (car region))
+         (end (cdr region))
+         (inhibit-read-only t))
+    (add-text-properties start end properties)))
+
+
+(defun ces-seng-rend-replace-timer-properties (point properties)  
+  (let* ((region (ces-seng-rend-next-time-region point))
+         (start (car region))
+         (end (cdr region))
+         (inhibit-read-only t))
+    (set-text-properties start end properties)))
+
+(defun ces-seng-rend-replace-timer-region (point message)  
+  (let* ((region (ces-seng-rend-next-time-region point))
+         (start (car region))
+         (end (cdr region))
+         (props (text-properties-at start))
+         (inhibit-read-only t))    
+      (goto-char start)
+      (delete-region start end)
+      (insert  message)
+      (set-text-properties start (+ start (length message)) props)))
+
+(defun ces-seng-rend-next-message (point)  
+  (let ((message-end (ces-seng-rend-end-of-message point)))
+    (ces-seng-rend-end-of-message message-end)))
+     
+(defun ces-seng-rend-previous-message (point)  
+  (let ((message-end (ces-seng-rend-end-of-message point)))
+     (ces-seng-rend-previous-prop-t message-end 'end-section)))
+
+(defun ces-seng-rend-next-time-region (point)  
+  (let* ((start (ces-seng-rend-next-prop-t point 'time-section))
+         (end (next-single-property-change start 'time-section)))
+    (cons start end)))
+
+(defun ces-seng-rend-message-region (point)
+  (let* ((end (ces-seng-rend-end-of-message point))
+         (start (or (previous-single-property-change end 'end-section) 1)))
+    (cons start end)))
+
+(defun ces-seng-rend-goto-next-byline (point)  
+  (let ((byline (ces-seng-rend-next-prop-t point 'byline-start)))
+    (when byline
+      (goto-char byline))))
+
+(defun ces-seng-rend-goto-previous-byline (point)  
+  (let ((byline (ces-seng-rend-previous-prop-t point 'byline-start)))
+    (when byline
+      (goto-char byline))))
+
+(defun ces-seng-rend-end-of-message (point)  
+  (ces-seng-rend-next-prop-t point 'end-section))
+
+(defun ces-seng-rend-next-prop-t (point prop)  
+  (let ((next-point (next-single-property-change point prop)))
+    (cond  ((not next-point) nil)           
+           ((not (get-text-property next-point prop))
+            (setq next-point (next-single-property-change next-point prop)))
+           (t next-point))))
+
+(defun ces-seng-rend-previous-prop-t (point prop)  
+  (let ((next-point (previous-single-property-change point prop)))
+    (cond  ((not next-point) nil)           
+           ((not (get-text-property next-point prop))
+            (setq next-point (previous-single-property-change next-point prop)))
+           (t next-point))))
 
 (provide 'ces-seng-reng)
 ;;; ces-seng-reng.el ends here
